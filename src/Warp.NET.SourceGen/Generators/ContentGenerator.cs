68aa98561750f6b2dc88687b1b29ddf68ec05008
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-11-17 14:48:46 +0100

    Stop using reflection to assign content

--- src/Warp.NET.SourceGen/Generators/ContentGenerator.cs
+++ src/Warp.NET.SourceGen/Generators/ContentGenerator.cs
@@ -9,6 +9,7 @@ public class ContentGenerator : ISourceGenerator
 {
 	private const string _namespace = $"%{nameof(_namespace)}%";
 	private const string _className = $"%{nameof(_className)}%";
+	private const string _contentFieldInitializers = $"%{nameof(_contentFieldInitializers)}%";
 	private const string _contentFields = $"%{nameof(_contentFields)}%";
 	private const string _contentProperties = $"%{nameof(_contentProperties)}%";
 	private const string _contentType = $"%{nameof(_contentType)}%";
@@ -20,8 +21,13 @@ public class ContentGenerator : ISourceGenerator
 
 		namespace {{_namespace}};
 
-		public static class {{_className}}
+		public sealed class {{_className}} : Warp.NET.IContentContainer<{{_contentType}}>
 		{
+			public static void Initialize(IReadOnlyDictionary<string, {{_contentType}}> content)
+			{
+				{{_contentFieldInitializers}}
+			}
+
 			{{_contentFields}}
 
 			{{_contentProperties}}
@@ -66,16 +72,23 @@ public class ContentGenerator : ISourceGenerator
 			.Replace(_namespace, gameNamespace)
 			.Replace(_className, className)
 			.Replace(_contentType, contentTypeName)
+			.Replace(_contentFieldInitializers, string.Join(Constants.NewLine, filePaths.ConvertAll(p => $"{GetFieldNameFromFileName(p)} = content.TryGetValue({GetPropertyNameFromFileName(p)}, out {contentTypeName}? {GetLocalNameFromFileName(p)}) ? {GetLocalNameFromFileName(p)} : null;")).IndentCode(2))
 			.Replace(_contentFields, string.Join(Constants.NewLine, filePaths.ConvertAll(p => $"private static {contentTypeName}? {GetFieldNameFromFileName(p)};")).IndentCode(1))
 			.Replace(_contentProperties, string.Join(Constants.NewLine, filePaths.ConvertAll(p => $"public static {contentTypeName} {GetPropertyNameFromFileName(p)} => {GetFieldNameFromFileName(p)} ?? throw new InvalidOperationException(\"Content does not exist or has not been initialized.\");")).IndentCode(1));
 
 		context.AddSource(className, SourceBuilderUtils.Build(sourceBuilder));
 	}
 
+	private static string GetLocalNameFromFileName(string filePath)
+	{
+		string name = Path.GetFileNameWithoutExtension(filePath);
+		return name.FirstCharToLowerCase();
+	}
+
 	private static string GetFieldNameFromFileName(string filePath)
 	{
-		string propertyName = GetPropertyNameFromFileName(filePath);
-		return $"_{propertyName.FirstCharToLowerCase()}";
+		string name = Path.GetFileNameWithoutExtension(filePath);
+		return $"_{name.FirstCharToLowerCase()}";
 	}
 
 	private static string GetPropertyNameFromFileName(string filePath)
--- src/Warp.NET/Bootstrapper.cs
+++ src/Warp.NET/Bootstrapper.cs
@@ -1,3 +1,4 @@
+using Warp.NET.Content;
 using Warp.NET.Content.Binaries;
 
 namespace Warp.NET;
@@ -16,15 +17,23 @@ public static class Bootstrapper
 	/// <param name="initialWindowFullScreen">Whether full screen should be enabled when the game starts.</param>
 	/// <param name="contentRootDirectory">The content root directory to generate a content file from. If the directory does not exist, or is <see langword="null" />, the file will not be generated.</param>
 	/// <param name="contentFilePath">The generated content file path required to bootstrap the game.</param>
-	/// <typeparam name="TGame">The game type which must implement <see cref="IGameBase{TSelf}"/>.</typeparam>
+	/// <typeparam name="TGame">The game type which must derive from <see cref="GameBase"/> and implement <see cref="IGameBase{TSelf}"/>.</typeparam>
 	/// <typeparam name="TBase">The base type which is used to access the game instance.</typeparam>
 	/// <typeparam name="TShaderUniformInitializer">The shader uniform initializer type. This type is generated.</typeparam>
+	/// <typeparam name="TModelContainer">The type containing the game's models. This type is generated.</typeparam>
+	/// <typeparam name="TShaderContainer">The type containing the game's shaders. This type is generated.</typeparam>
+	/// <typeparam name="TSoundContainer">The type containing the game's sounds. This type is generated.</typeparam>
+	/// <typeparam name="TTextureContainer">The type containing the game's textures. This type is generated.</typeparam>
 	/// <returns>The game instance.</returns>
-	/// <exception cref="InvalidOperationException">When the file at <paramref name="contentFilePath"/> does not exist, or when the entry assembly could not be retrieved.</exception>
-	public static TGame CreateGame<TGame, TBase, TShaderUniformInitializer>(string initialWindowTitle, int initialWindowWidth, int initialWindowHeight, bool initialWindowFullScreen, string? contentRootDirectory, string contentFilePath)
+	/// <exception cref="InvalidOperationException">When the file at <paramref name="contentFilePath"/> does not exist.</exception>
+	public static TGame CreateGame<TGame, TBase, TShaderUniformInitializer, TModelContainer, TShaderContainer, TSoundContainer, TTextureContainer>(string initialWindowTitle, int initialWindowWidth, int initialWindowHeight, bool initialWindowFullScreen, string? contentRootDirectory, string contentFilePath)
 		where TGame : GameBase, IGameBase<TGame>
 		where TBase : IBase<TGame>
-		where TShaderUniformInitializer : IShaderUniformInitializer, new()
+		where TShaderUniformInitializer : IShaderUniformInitializer
+		where TModelContainer : IContentContainer<Model>
+		where TShaderContainer : IContentContainer<Shader>
+		where TSoundContainer : IContentContainer<Sound>
+		where TTextureContainer : IContentContainer<Texture>
 	{
 		if (initialWindowFullScreen)
 			Graphics.CreateWindowFull(initialWindowTitle);
@@ -37,8 +46,11 @@ public static class Bootstrapper
 		if (!File.Exists(contentFilePath))
 			throw new InvalidOperationException("The generated content file is missing. Make sure to build in DEBUG mode or copy the file generated in DEBUG mode to the RELEASE output.");
 
-		// TODO: Assign the values from the decompiled content file.
 		DecompiledContentFile decompiledContentFile = ContentFileReader.Read(contentFilePath);
+		TModelContainer.Initialize(decompiledContentFile.Models);
+		TShaderContainer.Initialize(decompiledContentFile.Shaders);
+		TSoundContainer.Initialize(decompiledContentFile.Sounds);
+		TTextureContainer.Initialize(decompiledContentFile.Textures);
 
 		TShaderUniformInitializer.Initialize();
 
