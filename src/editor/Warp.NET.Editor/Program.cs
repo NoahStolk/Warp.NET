387c5479c1cc6f60fd03c41ac3ae6a075b0538c6
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-11-20 19:24:20 +0100

    Fix Warp.NET.Editor

--- src/editor/Warp.NET.Editor/Program.cs
+++ src/editor/Warp.NET.Editor/Program.cs
@@ -4,18 +4,18 @@ using Warp.NET.Editor;
 using Warp.NET.RenderImpl.Ui;
 using Warp.NET.RenderImpl.Ui.Rendering;
 
-#if DEBUG
-const string? contentRootDirectory = @"..\..\..\Content";
-#else
-const string? contentRootDirectory = null;
-#endif
-
 GameParameters gameParameters = new("Warp.NET Editor", Constants.InitialWindowWidth, Constants.InitialWindowHeight, false);
 
 OnChangeWindowSize = (w, h) => ViewportState.OnChangeWindowSize(w, h, gameParameters);
 Bootstrapper.CreateWindow(gameParameters);
 
-DecompiledContentFile decompiledContentFile = Bootstrapper.GetDecompiledContent(contentRootDirectory, "c");
+#if DEBUG
+const string? contentRootDirectory = @"..\..\..\..\..\lib\Warp.NET.RenderImpl.Ui\Content";
+#else
+const string? contentRootDirectory = null;
+#endif
+
+DecompiledContentFile decompiledContentFile = Bootstrapper.GetDecompiledContent(contentRootDirectory, "render-impl-ui-content");
 RenderImplUiCharsets.Initialize(decompiledContentFile.Charsets);
 RenderImplUiShaders.Initialize(decompiledContentFile.Shaders);
 RenderImplUiTextures.Initialize(decompiledContentFile.Textures);
