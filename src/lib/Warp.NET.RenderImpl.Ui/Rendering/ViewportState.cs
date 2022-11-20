9fe37cf8bbc3db52715d50f804b365535d41416a
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-11-20 22:16:01 +0100

    Remove default OnChangeWindowSize

--- src/lib/Warp.NET.RenderImpl.Ui/Rendering/ViewportState.cs
+++ src/lib/Warp.NET.RenderImpl.Ui/Rendering/ViewportState.cs
@@ -5,23 +5,11 @@ namespace Warp.NET.RenderImpl.Ui.Rendering;
 
 public static class ViewportState
 {
-	public static Viewport Viewport { get; private set; }
+	public static Viewport Viewport { get; set; }
 
-	public static Vector2 Offset { get; private set; }
+	public static Vector2 Offset { get; set; }
 
-	public static Vector2 Scale { get; private set; }
+	public static Vector2 Scale { get; set; }
 
 	public static Vector2 MousePosition => (Input.GetMousePosition() - Offset) / Scale;
-
-	public static void OnChangeWindowSize(int width, int height, GameParameters gameParameters)
-	{
-		float originalAspectRatio = gameParameters.InitialWindowWidth / (float)gameParameters.InitialWindowHeight;
-		float adjustedWidth = height * originalAspectRatio; // Adjusted for aspect ratio
-		Offset = new((width - adjustedWidth) / 2, 0);
-		Vector2 size = new(adjustedWidth, height); // Fix viewport to maintain aspect ratio
-		Scale = size / new Vector2(gameParameters.InitialWindowWidth, gameParameters.InitialWindowHeight);
-
-		Viewport = new((int)Offset.X, (int)Offset.Y, (int)size.X, (int)size.Y);
-		Gl.Viewport(Viewport.X, Viewport.Y, (uint)Viewport.Width, (uint)Viewport.Height);
-	}
 }
