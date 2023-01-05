ca315001b43875610ff238eedea0f13fcbed8030
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-01-05 23:00:16 +0100

    Remove TriangleRenderMode

--- src/lib/Warp.NET/Content/Mesh.cs
+++ src/lib/Warp.NET/Content/Mesh.cs
@@ -2,14 +2,12 @@ namespace Warp.NET.Content;
 
 public class Mesh
 {
-	public Mesh(Vertex[] vertices, uint[] indices, TriangleRenderMode triangleRenderMode)
+	public Mesh(Vertex[] vertices, uint[] indices)
 	{
 		Vertices = vertices;
 		Indices = indices;
-		TriangleRenderMode = triangleRenderMode;
 	}
 
 	public Vertex[] Vertices { get; }
 	public uint[] Indices { get; }
-	public TriangleRenderMode TriangleRenderMode { get; } // TODO: Remove.
 }
