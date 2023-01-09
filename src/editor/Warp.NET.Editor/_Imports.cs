5348d3310aff0bc1c181f83e460f866ebe28b715
Noah Stolk <noah.stolk@gmail.com>
2023-01-09 20:53:59 +0100

    Enable central package management (#37)
    * Enable central package management
    
    * Update dependencies
    
    * Remove editor project

--- src/editor/Warp.NET.Editor/_Imports.cs
+++
@@ -1,7 +0,0 @@
-global using static Warp.NET.Graphics;
-global using RenderImplUiCharsets = Warp.NET.RenderImpl.Ui.Charsets;
-global using RenderImplUiShaders = Warp.NET.RenderImpl.Ui.Shaders;
-global using RenderImplUiShaderUniformInitializer = Warp.NET.RenderImpl.Ui.ShaderUniformInitializer;
-global using RenderImplUiTextures = Warp.NET.RenderImpl.Ui.Textures;
-global using Shader = Warp.NET.Content.Shader;
-global using Texture = Warp.NET.Content.Texture;
